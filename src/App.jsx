c6adb0485d71bd62adba922fc4307358a5d09401
VarunJain1402 <varunjain1402@gmail.com>
2023-05-13 18:16:55 +0530

    Added user store using zustand for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+import { onAuthStateChanged } from 'firebase/auth'
 import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import List from './components/list/List'
-import Login from './components/login/Login'  
+import Login from './components/login/Login'
 const App = () => {
-  const user =false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, user => {
+      fetchUserInfo(user?.uid)
+    })
+
+    return () => {
+      unSub()
+    }
+  }, [fetchUserInfo])
+
+  if (isLoading) return <div className="loading">Loading...</div>;
   return (
     <div className='container'>
-      {user ?(
-       <>
-       <List />
-      <Chat />
-      <Detail />
-       </> 
-      ):(<Login/>)}
-      
+      {currentUser ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   )
 }
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "firebase/auth";
+  import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/upload";
 const Login = () => {
   const [avatar, setAvatar] = useState({
     file: null,
@@ -26,6 +32,41 @@ const Login = () => {
       setLoading(false);
     }
   };
+
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
+
+
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+        const imgUrl = await upload(avatar.file);
+  
+        await setDoc(doc(db, "users", res.user.uid), {
+          username,
+          email,
+          avatar: imgUrl,
+          id: res.user.uid,
+          blocked: [],
+        });
+  
+        await setDoc(doc(db, "userchats", res.user.uid), {
+          chats: [],
+        });
+  
+        toast.success("Account created! You can login now!");
+      } catch (err) {
+        console.log(err);
+        toast.error(err.message);
+      } finally {
+        setLoading(false);
+      }
+  }
 
   return (
     <div className="login">
